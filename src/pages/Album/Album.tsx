2a483044d849adccafc2bf8b891ec9d25a6f470a
vornies <jarrodgaluzen@gmail.com>
2023-05-31 10:35:55 +1200

    updated with some modal tests

--- src/pages/Album/Album.tsx
+++ src/pages/Album/Album.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Album.css'
 import fetchPhotoData, { Picture } from '../../services/api'
 import paginate from '../../utils/pagination'
