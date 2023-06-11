6132d5cab274038becf698cb319a075fb15c638a
vornies <jarrodgaluzen@gmail.com>
2023-06-11 19:10:47 +1200

    fixes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,6 +7,49 @@ import { setPaginatedData } from '../../state/paginationSlice'
 import paginate from '../../utils/pagination'
 import { setModalOpen, setModalPicture } from '../../state/modalSlice'
 
+const Card = ({ picture }: { picture: Picture }) => {
+	const [isLoading, setIsLoading] = useState(true)
+	const [isError, setIsError] = useState(false)
+	const dispatch = useDispatch<AppDispatch>()
+
+	const handleImageLoad = () => {
+		setIsLoading(false)
+	}
+
+	const handleImageError = () => {
+		setIsLoading(false)
+		setIsError(true)
+	}
+
+	const handleCardClick = () => {
+		dispatch(setModalPicture(picture))
+		dispatch(setModalOpen(true))
+	}
+
+	return (
+		<div className="card" onClick={handleCardClick}>
+			{isError && !isLoading && (
+				<div className="error-text" data-testid={picture.title}>
+					<p>Error loading image</p>
+				</div>
+			)}
+			<div className="img-container">
+				{isLoading && <p className="loading-text">Loading.....</p>}
+				<img
+					src={picture.thumbnailUrl}
+					alt={picture.title}
+					onLoad={handleImageLoad}
+					onError={handleImageError}
+				/>
+			</div>
+			<div className="card-content">
+				<h5 className="card-title">{picture.title}</h5>
+				<p className="card-album-id">Album ID: {picture.albumId}</p>
+			</div>
+		</div>
+	)
+}
+
 const Cards = () => {
 	const dispatch = useDispatch<AppDispatch>()
 	const { data, status } = useSelector((state: RootState) => state.photoData) // Access the state from Redux
@@ -19,62 +62,10 @@ const Cards = () => {
 		dispatch(setPaginatedData(paginate(data)))
 	}, [dispatch, data])
 
-	const Card = (picture: Picture, index: number) => {
-		const [isLoading, setIsLoading] = useState(true)
-		const [isError, setIsError] = useState(false)
-		const dispatch = useDispatch<AppDispatch>()
-
-		const handleImageLoad = () => {
-			setIsLoading(false)
-		}
-
-		const handleImageError = () => {
-			setIsLoading(false)
-			setIsError(true)
-		}
-
-		const handleCardClick = () => {
-			dispatch(setModalPicture(picture))
-			dispatch(setModalOpen(true))
-		}
-
-		return (
-			<div className="card" onClick={handleCardClick} key={index}>
-				{isError && !isLoading && (
-					<div className="error-text" data-testid={picture.title}>
-						<p>Error loading image</p>
-					</div>
-				)}
-				<div className="img-container">
-					{isLoading && <p className="loading-text">Loading.....</p>}
-					<img
-						src={picture.thumbnailUrl}
-						alt={picture.title}
-						onLoad={handleImageLoad}
-						onError={handleImageError}
-					/>
-				</div>
-				<div className="card-content">
-					<h5 className="card-title">{picture.title}</h5>
-					<p className="card-album-id">Album ID: {picture.albumId}</p>
-				</div>
-			</div>
-		)
-	}
-
 	return (
 		<div className="card-container">
 			{status === 'succeeded' &&
-				currentData.map((picture: Picture, index) => (
-					<Card
-						key={index}
-						albumId={picture.albumId}
-						id={picture.id}
-						title={picture.title}
-						thumbnailUrl={picture.thumbnailUrl}
-						url={picture.url}
-					/>
-				))}
+				currentData.map((picture: Picture, index) => <Card key={index} picture={picture} />)}
 		</div>
 	)
 }
